4299f6ca20933e9c0a676a44fe98c9f0574526db
Hiroshi Urabe <mail@torounit.com>
2020-01-18 15:38:05 +0900

    :tada: build on netlify

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/torounit/gridsome-shifter)
 
 @see [https://www.digitalcube.jp/shifter/4434/](https://www.digitalcube.jp/shifter/4434/)
+
+```bash
+export SHIFTER_SITE_ID=your-shifter-site-id
+export SHIFTER_PASS=your-passowrd
+export SHIFTER_USER=shifter@exapmple.org
+pipenv install
+bash ./build.sh
+```
--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
-env
-
-if [ "${INCOMING_HOOK_BODY}" == "" ] ; then
-  echo 'No webhook'
-  exit 0
-fi
-
-echo "${INCOMING_HOOK_BODY}" | jq -r .CONTAINER_URL
-export CONTAINER_URL=$(echo "${INCOMING_HOOK_BODY}" | jq -r .CONTAINER_URL)
+echo "start shifter"
+RESULT=$(python3 ./shifter/run.py start)
+echo $RESULT
+echo "parse url"
+export CONTAINER_URL=$(node ./shifter/getContainerURL.js "$RESULT")
+echo "shifter: $CONTAINER_URL"
+echo "build"
 npm run build
+
