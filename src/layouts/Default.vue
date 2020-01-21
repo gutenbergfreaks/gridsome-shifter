eb54e00f209712e106b2d7f0691d4ce3d828f7ae
miminari <mimitips@gmail.com>
2020-01-21 21:11:37 +0900

    :wrench: Update css rules

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,54 +1,14 @@
 <template>
-  <div class="layout">
-    <header class="header">
+  <div class="l-wrapper">
+    <header class="l-global--header">
       <h1>
         <g-link to="/">Gutenberg Freaks</g-link>
       </h1>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+      <nav class="m-nav">
+        <g-link class="m-nav__link" to="/">Home</g-link>
+        <g-link class="m-nav__link" to="/about/">About</g-link>
       </nav>
     </header>
     <slot/>
   </div>
 </template>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-}
-
-img {
-  max-width: 100%;
-}
-
-nav[role="navigation"] {
-  text-align: center;
-}
-nav[role="navigation"] a {
-  display: inline-block;
-  margin: 1.0em 0.75em 2.0em;
-}
-
-.layout {
-  max-width: 600px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-.header {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-}
-.nav__link {
-  margin-left: 20px;
-}
-.post-list {
-  list-style: none;
-  padding-left: 0;
-}
-.post-list li {
-  padding: 1em 0;
-}
-</style>
